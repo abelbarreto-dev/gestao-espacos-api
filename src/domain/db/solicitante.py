c8096855ecd000b7ff9db5f222ecc48ac5142f03
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-12 13:28:37 -0300

    feat: class solicitante database representation

---
+++ src/domain/db/solicitante.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.domain.db.base import Base
+
+
+class Solicitante(Base):
+    __tablename__ = "solicitante"
+
+    id_solicitante: Mapped[int] = mapped_column(primary_key=True)
+
+    nome: Mapped[str] = mapped_column(String(100), nullable=False)
+    tipo: Mapped[str] = mapped_column(String(20), nullable=False)
+    documento: Mapped[str] = mapped_column(String(20), nullable=False, unique=True)
+    contato: Mapped[str] = mapped_column(String(50), nullable=False)
