30d97dafb08e5384bbde723b952d53600c96837c
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-12 13:37:20 -0300

    feat: class solicitacao database representation

---
+++ src/domain/db/solicitacao.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from sqlalchemy import String, DateTime
+from sqlalchemy.orm import mapped_column, Mapped
+
+from src.domain.db.base import Base
+
+
+class Solicitacao(Base):
+    __tablename__ = "solicitacao"
+
+    id_solicitacao: Mapped[int] = mapped_column(primary_key=True)
+
+    id_solicitante: Mapped[int] = mapped_column(nullable=False)
+    id_usuario: Mapped[int] = mapped_column(nullable=False)
+    id_espaco: Mapped[int] = mapped_column(nullable=False)
+    id_tipo_evento: Mapped[int] = mapped_column(nullable=False)
+
+    status: Mapped[int] = mapped_column(String(20), nullable=False, default="pendente")
+
+    data_solicitacao: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now())
