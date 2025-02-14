be7d77bb9ac0be91074ea6904efca2620b5b3b13
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-14 15:03:15 -0300

    feat: a response builder created

--- src/response/response.py
+++ src/response/response.py
@@ -1,8 +1,20 @@
-from fastapi.openapi.models import Response
+from fastapi import status
+from fastapi.responses import JSONResponse
+from typing import Any, Dict, Optional
 
 
-def response_build(content: any) -> Response:
-    return Response(
-        description="Status OK",
-        content=content,
-    )
+def response_builder(
+    status_code: int = status.HTTP_200_OK,
+    message: str = "Operation Ok",
+    data: Optional[Any] = None,
+    meta: Optional[Dict[str, Any]] = None,
+    status_type: str = "success"
+) -> JSONResponse:
+    response_data = {
+        "status": status_type,
+        "message": message,
+        "data": data if data is not None else {},
+        "meta": meta if meta is not None else {}
+    }
+
+    return JSONResponse(content=response_data, status_code=status_code)
