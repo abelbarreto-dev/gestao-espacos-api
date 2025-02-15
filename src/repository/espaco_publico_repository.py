3e057696f316a2df10ef69a2c2291a2d2344ddec
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-14 21:40:55 -0300

    fix: espaco publico repository

--- src/repository/espaco_publico_repository.py
+++ src/repository/espaco_publico_repository.py
@@ -8,29 +8,23 @@ from src.domain.db.espaco_publico import EspacoPublico
 
 class EspacoPublicoRepository:
     def __init__(self):
-        pass
+        self.engine = sync_engine()
 
     def create_espaco_publico(self, espaco_publico: EspacoPublico) -> EspacoPublico:
-        engine = sync_engine()
-
-        with Session(engine) as session:
+        with Session(self.engine) as session:
             session.add(espaco_publico)
             session.commit()
 
         return espaco_publico
 
     def find_all_espaco_publicos(self) -> List:
-        engine = sync_engine()
-
-        with Session(engine) as session:
+        with Session(self.engine) as session:
             all_espacos_publicos = session.query(EspacoPublico).all()
 
         return all_espacos_publicos
 
     def find_espaco_publico_by_id(self, id: int) -> EspacoPublico:
-        engine = sync_engine()
-
-        with Session(engine) as session:
+        with Session(self.engine) as session:
             espaco_publico = session.query(EspacoPublico).filter_by(id_usuario=id).first()
 
         if not espaco_publico:
@@ -39,26 +33,24 @@ class EspacoPublicoRepository:
         return espaco_publico()
 
     def update_espaco_publico(self, espaco_publico: EspacoPublico, id: int) -> EspacoPublico:
-        engine = sync_engine()
-
-        with Session(engine) as session:
+        with Session(self.engine) as session:
             new_esp_publico = session.query(EspacoPublico).filter_by(id_espaco=id).first()
 
-        if not new_esp_publico:
-            raise Exception("Espaço Público not found")
+            if not new_esp_publico:
+                raise Exception("Espaço Público not found")
 
-        new_esp_publico.nome = espaco_publico.nome
-        new_esp_publico.endereco = espaco_publico.endereco
-        new_esp_publico.descricao = espaco_publico.descricao
-        new_esp_publico.capacidade = espaco_publico.capacidade
-        new_esp_publico.disponibilidade = espaco_publico.disponibilidade
+            new_esp_publico.nome = espaco_publico.nome
+            new_esp_publico.endereco = espaco_publico.endereco
+            new_esp_publico.descricao = espaco_publico.descricao
+            new_esp_publico.capacidade = espaco_publico.capacidade
+            new_esp_publico.disponibilidade = espaco_publico.disponibilidade
+
+            session.commit()
 
         return new_esp_publico()
 
     def delete_espaco_publico_by_id(self, id: int) -> Any:
-        engine = sync_engine()
-
-        with Session(engine) as session:
+        with Session(self.engine) as session:
             espaco_publico = session.query(EspacoPublico).filter_by(id_espaco=id).first()
 
             if not espaco_publico:
