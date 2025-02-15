a68a20dba9fc843c04288ad2fef6571fcbe567f5
abelbarreto-dev <abelbcarvalho.dev@gmail.com>
2025-02-15 13:23:51 -0300

    feat: solicitacao routes are ready

---
+++ src/routes/solicitacao_routes.py
@@ -0,0 +1,35 @@
+from typing import Any
+
+from fastapi import APIRouter
+
+from src.controllers.solicitacao_controller import SolicitacaoController
+from src.domain.dtos.solicitacao import Solicitacao
+
+
+solicit_controller = SolicitacaoController()
+solicitacao_routes = APIRouter()
+
+
+@solicitacao_routes.post("/solicitacoes")
+def create_solicitacao(solicitaco: Solicitacao) -> Any:
+    return solicit_controller.create_solicitacao(solicitaco)
+
+
+@solicitacao_routes.get("/solicitacoes")
+def find_all_solicitacaos() -> Any:
+    return solicit_controller.find_all_solicitacaos()
+
+
+@solicitacao_routes.get("/solicitacoes/{id}")
+def find_solicitacao_by_id(id: int) -> Any:
+    return solicit_controller.find_solicitacao_by_id(id)
+
+
+@solicitacao_routes.put("/solicitacoes/{id}")
+def update_solicitacao(solicitacao: dict, id: int) -> Any:
+    return solicit_controller.update_solicitacao(solicitacao, id)
+
+
+@solicitacao_routes.delete("/solicitacoes/{id}")
+def delete_solicitacao_by_id(id: int) -> Any:
+    return solicit_controller.delete_solicitacao_by_id(id)
